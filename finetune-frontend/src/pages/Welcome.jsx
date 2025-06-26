b9cc1c1d19dc65886bbb1e23178cad8aed5cadbf
Daniel Palmer <dmpalmer@meta.com>
2025-06-25 20:35:25 -0700

    missed commit from previous pr

--- finetune-frontend/src/pages/Welcome.jsx
+++ finetune-frontend/src/pages/Welcome.jsx
@@ -1,34 +1,60 @@
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Input } from '@/components/ui/input'
+import { Input } from '@/components/ui/input';
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogOverlay,
+    DialogPortal,
+    DialogTitle,
+    DialogTrigger
+} from '@/components/ui/dialog'
+
 
 const WelcomePage = () => {
+    const [showRegDialog, setShowRegDialog] = useState(false);
 
     const handleLogin = (e) => {
         e.preventDefault();
     }
 
-    const handleRegister = (e) => {
-        console.log('register');
-    }
 
     return (
         <div className='flex flex-col'>
-            <div className='m-10'>
-                <h1 className='text-8xl font-semibold '> FineTune</h1>
-                {/*Icon placeholder*/}
-            </div>
-            <div>
-                <form className='flex flex-col gap-5' onSubmit={handleLogin}>
-                    <Input placeholder="username" required/>
-                    <Input placeholder="password" type='password' required/>
-                    <div className='flex gap-5'>
-                        <Button type='button' variant='outline' size='lg' onClick={handleRegister} className='text-orange !border-orange flex-1 hover:text-darkpurple hover:!bg-orange focus:scale-105 active:scale-105'>
-                            Register</Button>
-                        <Button type='submit' variant='outline' size='lg' className='text-indigo !border-indigo flex-1 hover:text-darkpurple hover:!bg-indigo focus:scale-105 active:scale-105'>
-                            Login</Button>
-                    </div>
-                </form>
-            </div>
+            <Dialog open={showRegDialog} onOpenChange={setShowRegDialog}>
+                <div className='m-10'>
+                    <h1 className='text-8xl font-semibold '> FineTune</h1>
+                    {/*Icon placeholder*/}
+                </div>
+                <div>
+                    <form className='flex flex-col gap-5' onSubmit={handleLogin}>
+                        <Input placeholder="username" required/>
+                        <Input placeholder="password" type='password' required/>
+                        <div className='flex gap-5'>
+                            <DialogTrigger asChild >
+                                <Button type='button' variant='outline' size='lg' 
+                                className='text-orange !border-orange flex-1 hover:text-darkpurple hover:!bg-orange focus:scale-105 active:scale-105'>
+                                    Register</Button>
+                            </DialogTrigger>
+                            <Button type='submit' variant='outline' size='lg' 
+                            className='text-indigo !border-indigo flex-1 hover:text-darkpurple hover:!bg-indigo focus:scale-105 active:scale-105'>
+                                Login</Button>
+                        </div>
+                    </form>
+                </div>
+                <Dialog open={showRegDialog} onOpenChange={setShowRegDialog}></Dialog>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Register User</DialogTitle>
+                        <DialogDescription></DialogDescription>
+                    </DialogHeader>
+                    
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
