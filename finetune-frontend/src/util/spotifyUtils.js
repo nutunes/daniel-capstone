12b5ef9905a5950b94bc5710eaa68493c8f08590
Daniel Palmer <dmpalmer@meta.com>
2025-07-02 10:57:59 -0700

    songs from a users spotify are uploaded to the database

--- finetune-frontend/src/util/spotifyUtils.js
+++ finetune-frontend/src/util/spotifyUtils.js
@@ -18,6 +18,7 @@ const uploadUsersTop500Tracks = async(token) => {
             //     throw new Error('fetch songs fail')
             // }
             const responseJSON = await response.json();
+            console.log(responseJSON);
             spotifyTracks = [...spotifyTracks, ...responseJSON.items];
             if (responseJSON.next === null){
                 //Don't keep searching for more songs if you have no more top items
@@ -27,10 +28,29 @@ const uploadUsersTop500Tracks = async(token) => {
     } catch(error){
         console.error(error);
     }
-
+    console.log('spotify tracks: ' + spotifyTracks)
     //Upload tracks to the db
     try {
-
+        for ( track of spotifyTracks){
+            const response = await fetch(`http://127.0.0.1:3000/spotify/add_song`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    like: true,
+                    spotify_id: track.id,
+                    title: track.name,
+                    album: track.album.name,
+                    spotify_album_id: track.album.idm
+                }),
+                credentials: 'include',
+            })
+            // Commented out for same strict mode issue
+            // if (!response.ok){
+            //     throw new Error('failed to add song');
+            // }
+        }
     } catch(error){
         console.error(error);
     }
