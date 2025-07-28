cdd99a563e97371b0da856214c4d0f68a82f8a8d
Daniel Palmer <dmpalmer@meta.com>
2025-07-28 14:36:23 -0700

    spacing for friends page

--- finetune-frontend/src/pages/Friends.jsx
+++ finetune-frontend/src/pages/Friends.jsx
@@ -9,9 +9,10 @@ const Friends = () => {
     return (
         <div className='flex flex-col flex-1 w-full relative'>
             <LoggedInHeader page='Friends'/>
-            <div className='flex-1 flex flex-col justify-center items-center'>
-                <p className='font-fredoka'>Friends Page Placeholder</p>
-                <p className='font-fredoka'>{user}</p>
+            <div className='flex-1 flex flex-row justify-around items-start m-20'>
+                <p className='font-fredoka'>Friends</p>
+                <p className='font-fredoka'>Recommended</p>
+                <p className='font-fredoka'>Requests</p>
             </div>
         </div>
     )
